a6003e45b71a9dcd8e3d84ba13d3635e248c90ee
nktexier <nktexier@gmail.com>
2021-11-16 21:15:49 -0300

    JS 2
    formulario arreglado

--- SprintGrupal_1/assets/js/main.js
+++ SprintGrupal_1/assets/js/main.js
@@ -1,2 +1,30 @@
 let r = new Date();
 document.getElementById("Fecha").innerHTML=r;
+
+document.getElementById('boton').addEventListener('click', validarFormulario)
+// Funcion que valida el ingreso de los datos en el formulario de contacto
+function validarFormulario (e) {
+    
+    e.preventDefault()
+    //Variables
+      let lfname = document.getElementById("lfname").value;
+      let lemail = document.getElementById("lemail").value;
+      let lmessage = document.getElementById("lmessage").value;
+    
+      console.log("Nombre " + lfname);
+      console.log("Correo" + lemail);
+      console.log("Mensaje" + lmessage);
+      
+     // Realizamos las validaciones de los Datos
+     if ((lfname.length ==0) || (lemail.length==0) || (lmessage.length==0))
+       {
+         alert('Completar los datos');
+       }
+       else
+       {
+        alert('Hemos recibido su mensaje, nuestro equipo se pondrá en contacto con usted.');
+       }
+    
+    };
+   
+    

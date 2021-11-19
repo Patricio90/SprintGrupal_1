c5730771fd420a51405338310f1899c2c0352dae
spicke23 <angelo.valenzuela@alumnos.uta.cl>
2021-11-18 23:37:55 -0300

    Revision previa
    Modificacion de main.js y contacto.html

--- SprintGrupal_1/assets/js/main.js
+++ SprintGrupal_1/assets/js/main.js
@@ -1,6 +1,7 @@
 let r = new Date();
 document.getElementById("Fecha").innerHTML=r;
 
+<<<<<<< Updated upstream
 let boton = document.getElementById('boton')
 
 if(boton){
@@ -36,3 +37,27 @@ function validarFormulario (e) {
     $(document).ready(function () {
       $('#tablaProductos').DataTable();
     });
+=======
+function validarFormulario(){
+    //Variables
+      let lfname = document.getElementById('lfname').value
+      let lemail = document.getElementById('lemail').value
+      let lmessage = document.getElementById('lmessage').value;
+    
+//      console.log("Nombre " + lfname);
+//      console.log("Correo" + lemail);
+//      console.log("Mensaje" + lmessage);
+    
+    // Realizamos las validaciones de los Datos
+     if ((lfname.length.value == 0) || (lemail.length.value == 0) || (lmessage.length.value == 0)){
+         alert('Aprende a leer y llena los datos completos');
+     }
+       else{
+           alert('Hemos recibido su mensaje, nuestro equipo se pondrá en contacto con usted.');
+       }
+}
+
+function CambiaBorde(){
+    document.getElementById('lfname').style.borderColor = "#FF0000"
+}
+>>>>>>> Stashed changes

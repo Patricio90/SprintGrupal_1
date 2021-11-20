4bfe25f4a9eff5b4020c47b96f2ff5a349e7531b
Patricio90 <patricio_val@msn.com>
2021-11-19 22:13:36 -0300

    Update

--- SprintGrupal_1/assets/js/main.js
+++ SprintGrupal_1/assets/js/main.js
@@ -1,2 +1,31 @@
 let r = new Date();
 document.getElementById("Fecha").innerHTML=r;
+
+// Funcion que valida el ingreso de los datos en el formulario de contacto
+function validarFormulario () {
+    //Variables
+      let lfname = document.getElementById("lfname").value;
+      let lemail = document.getElementById("lemail").value;
+      let lmessage = document.getElementById("lmessage").value;
+    
+      console.log("Nombre " + lfname);
+      console.log("Correo" + lemail);
+      console.log("Mensaje" + lmessage);
+      
+     // Realizamos las validaciones de los Datos
+     if ((lfname.length ==0) || (lemail.length==0) || (lmessage.length==0))
+       {
+         alert('Aprende a leer y llena los datos completos');
+       }
+       else
+       {
+        alert('Hemos recibido su mensaje, nuestro equipo se pondrá en contacto con usted.');
+       }
+    
+    };
+
+    $(document).ready(function () {
+        $('#example').DataTable();
+      });let
+    
+
